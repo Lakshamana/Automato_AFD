2aba31540565fa28b1e96cea53ef1fbf79d124ca
Lakshamana <guitrompa1@gmail.com>
2017-08-16 19:06:49 -0300

    Update biblio.h

--- biblio.h
+++ biblio.h
@@ -58,6 +58,16 @@ int empty_alfa(Alfabeto *ptr){
     return (ptr == NULL) ? 1 : 0;
 }
 
+void enq_w(Alfabeto **ptr, char data){
+    Alfabeto *p1 = NULL;
+    p1 = malloc((sizeof(Alfabeto)));
+    if(p1 != NULL){
+        p1->carac = data;
+        p1->prox = *ptr;
+        *ptr = p1;
+    }
+}
+
 typedef struct est{
     char e[5];
     struct est *prox;
@@ -66,6 +76,7 @@ typedef struct est{
 /*
 *   int seek_est(Estado *ptr, char *data);
 *   void add_est(Estado **ptr, char *data);
+*   void enq_est(Estado **ptr, char *data); -> Usado somente para enfileirar estados correntes na leitura (criar outra estrutura).
 *   void show_est(Estado *ptr);
 *   void empty_alfa(Estado *ptr);
 *   void read_est(FILE **file, Estado **ptr);
@@ -105,9 +116,19 @@ void add_est(Estado **ptr, char *data){
     }
 }
 
+void enq_est(Estado **ptr, char *data){
+    Estado *p1 = NULL;
+    p1 = malloc(sizeof(Estado));
+    if(p1 != NULL){
+        strcpy(p1->e, data);
+        p1->prox = *ptr;
+        *ptr = p1;
+    }
+}
+
 void show_est(Estado *ptr){
     while(ptr != NULL){
-        printf("%s", ptr->e);
+        printf("%s ", ptr->e);
         ptr = ptr->prox;
     }
 }
@@ -157,8 +178,8 @@ typedef struct rule{
 *   int empty_trule(TRule *ptr);
 *   int al1final(TRule *p1, Estado *p2);
 *   void show_trule(TRule *ptr);
-*   seek_trule(TRule *ptr);
-*   read_trule(FILE **file, TRule **ptr);
+*   int seek_trule(TRule *ptr);
+*   void read_trule(FILE **file, TRule **ptr);
 */
 
 void add_trule(TRule **ptr, char *ei, char data, char *eo){
@@ -209,7 +230,7 @@ int seek_trule(TRule *ptr, char *ei, char c, char *eo){
     return 0;
 }
 
-int al1final(TRule *p1, Estado *p2){
+int al1final(TRule *p1, Estado *p2_fin){
     if(p1 != NULL)
         while(p1 != NULL){
             if(seek_est(p2, p1->e_out)) //A lista de estados especificada (p2) é a lista de estados finais.
@@ -219,9 +240,63 @@ int al1final(TRule *p1, Estado *p2){
     return 0;
 }
 
-void read_trule(FILE **file, TRule **ptr){
-    char ei[5], c, eo[5];
-    while(fscanf(*file, "%s %s %s", ei, c, eo) != EOF){
+void cutcommas(char *s){
+    s[strlen(s) - 1] = '\0';
+}
 
+void read_trule(FILE **file, TRule **p1, Alfabeto *p2, Estado *p3){
+    char ei[5], c[2], eo[5];
+    while(fscanf(*file, "%s %s %s", ei, c, eo) != EOF){
+        cutcommas(ei);
+        cutcommas(c);
+        if(!seek_trule(*ptr, ei, c, eo) && seek_alfa(p2, c[0]) && seek_est(p3, ei) && seek_est(p3, eo))
+            add_trule(p1, ei, c[0], eo);
     }
+}
+
+/*
+*   int read_word_user(Alfabeto **w, Alfabeto *ptr);
+*   char * next_state(TRule *ptr, char *ei, char c);
+*   int automate(Alfabeto **w, Alfabeto *alfa, Estado **current);
+*/
+
+int read_user_word(Alfabeto **w, Alfabeto *ptr){
+    char read = ' ';
+    if(read != '\n')
+        while(read != '\n'){
+            read = getchar();
+            if(seek_alfa(ptr, read))
+                add_alfa(w, read);
+            else
+                return 0;
+        }
+    return 1;
+}
+
+char * next_state(TRule *ptr, char *ei, char c){
+    while(ptr != NULL){
+        if(strcmp(ptr->e_in, ei) == 0 && ptr->carac == c)
+            return ptr->e_out;
+        ptr = ptr->prox;
+    }
+    return NULL;
+}
+
+int automate(Alfabeto *w, TRule *rule, Estado **current, Estado *fin){
+    while(w != NULL){
+        char read = w->carac;
+        char current_state = current->e;
+        char *new_state = next_state(rule, current_state, read);
+        if(new_state != NULL)
+            add_est(current, new_state);
+        else return 0;
+        w = w->prox;
+    }
+    Estado *ptr = NULL;
+    ptr = *current;
+    while(ptr->prox != NULL)
+        ptr = ptr->prox;
+    if(seek_est(fin, ptr->e))
+       return 1;
+    else return 0;
 }
